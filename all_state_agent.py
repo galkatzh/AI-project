f5696070c04cf663b4d26f9c33822d578c998a91
Omri <beomri@gmail.com>
2018-08-07 13:16:40 +0300

    q-learning using full states

---
+++ all_state_agent.py
@@ -0,0 +1,100 @@
+from pommerman.agents import BaseAgent
+import numpy as np
+import random
+import pommerman.utility as util
+import pommerman.constants as consts
+import os.path
+
+
+#TODO:
+    # -  make sure shown state is cur_state
+    # - where powerup
+    # - check if enemies can escape
+    # - use reward for killing other players
+    
+filename = 'qvalues.npy'
+dirs = [1,2,3,4]  #up, down, left, right    
+
+def flip_coin(p):
+    r = random.random()
+    return r > p
+
+def freeze(d):
+    if isinstance(d, dict):
+        return frozenset((key, freeze(value)) for key, value in d.items())
+    elif isinstance(d, np.ndarray):
+        return tuple(d.flatten())
+    elif isinstance(d, list):
+        return tuple(freeze(value) for value in d)
+    return d
+
+class NewAgent(BaseAgent):
+
+    def __init__(self, *args, **kwargs):
+        super(NewAgent, self).__init__(*args, **kwargs)
+        self.last_action = 0
+        self.new_action = 0
+        self.cur_state = (0,)
+        self.last_state = (0,)
+        self.epsilon = 0.8
+        self.discount = 1
+        self.alpha = 1
+        self.done = False
+        self.q_values = dict()
+        if os.path.isfile(filename):
+            self.q_values = np.load(filename).item()
+            
+    def save_qvalues(self):
+        np.save(filename, self.q_values)
+
+    def extract_state(self, obs):
+#        dirs = [1,2,3,4]  #up, down, left, right
+        return freeze(obs)
+            
+    def set_start_state(self, obs):
+        self.cur_state = self.extract_state(obs)
+
+    def update_q_value(self, reward, new_state = None, old_state = None, last_action=None):
+        if new_state == None or old_state == None or last_action==None:
+            if self.done:
+                return
+            if self.cur_state not in self.q_values:
+                self.q_values[self.cur_state] = [0] * 6
+            if self.last_state not in self.q_values:
+                self.q_values[self.last_state] = [0] * 6
+            best_next_action = np.argmax(self.q_values[self.cur_state])    
+            td_target = reward + self.discount * self.q_values[self.cur_state][best_next_action]
+            td_delta = td_target - self.q_values[self.last_state][self.last_action]
+            self.q_values[self.last_state][self.last_action] += self.alpha * td_delta
+            if reward != 0:
+                self.episode_end();
+        else:
+            if new_state not in self.q_values:
+                self.q_values[new_state] = [0] * 6
+            if old_state not in self.q_values:
+                self.q_values[old_state] = [0] * 6
+            best_next_action = np.argmax(self.q_values[new_state])    
+            td_target = reward + self.discount * self.q_values[new_state][best_next_action]
+            td_delta = td_target - self.q_values[old_state][last_action]
+            self.q_values[old_state][last_action] += self.alpha * td_delta
+        
+
+    def act(self, obs, action_space):
+#        import IPython
+#        IPython.embed()
+        self.done = False
+        self.last_action = self.new_action
+        self.last_state = self.cur_state
+        self.cur_state = self.extract_state(obs)
+        if self.extract_state(obs) in self.q_values and flip_coin(self.epsilon):
+            self.new_action = np.argmax(self.q_values[self.extract_state(obs)])
+        else:
+            self.new_action = action_space.sample()
+        return self.new_action
+
+    def episode_end(self):
+        self.done = True
+        self.last_action = 0
+        self.new_action = 0
+        self.cur_state = (0,)
+        self.last_state = (0,)
---
+++ train_q_full_state.py
@@ -0,0 +1,68 @@
+import pommerman
+from pommerman import agents
+from all_state_agent import NewAgent
+from random import randint
+
+DEBUG = True
+
+def main():
+    
+    # Print all possible environments in the Pommerman registry
+    print(pommerman.REGISTRY)
+    
+    bla = NewAgent()
+
+    # Create a set of agents (exactly four)
+    agent_list = [
+        agents.SimpleAgent(),
+        agents.SimpleAgent(),
+        agents.SimpleAgent(),
+        agents.SimpleAgent(),
+#        agents.RandomAgent(),
+        #agents.RandomAgent(),
+#        bla
+        # agents.DockerAgent("pommerman/simple-agent", port=12345),
+    ]
+    learner_index = randint(0,3)
+    agent_list[learner_index] = bla
+    # Make the "Free-For-All" environment using the agent list
+    env = pommerman.make('PommeTeamCompetition-v0', agent_list)
+    reward = [0,0,0,0]
+
+    # Run the episodes just like OpenAI Gym
+    for i_episode in range(50):
+        state = env.reset()
+#        import IPython
+#        IPython.embed()
+        cur_obs = env.get_observations()
+        bla.set_start_state(cur_obs[learner_index])
+        done = False
+        while not done:
+#                input()
+            actions = env.act(state)
+            
+            
+            state, reward, done, info = env.step(actions)
+            last_obs = cur_obs
+            cur_obs = env.get_observations()
+            for i in range(4):
+                if i != learner_index:
+                    bla.update_q_value(reward[i],new_state = bla.extract_state(cur_obs[i]),
+                                       old_state = bla.extract_state(last_obs[i]), last_action=actions[i])
+            
+            bla.update_q_value(reward[learner_index])
+            
+            if DEBUG and reward[learner_index] == 0:
+                env.render()
+                print(bla.cur_state)
+#                input()
+            
+            #print(reward, done, info)
+#        print(bla.q_values)
+        print('Episode {} finished with rewards: {}'.format(i_episode, reward))
+    bla.save_qvalues()
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
