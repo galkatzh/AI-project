6282c292a8fb5ae5b1a7329b7d2a188fe637a637
bgalb <gal.katzhendler@mail.huji.ac.il>
2018-08-21 13:42:00 +0300

    MCTS OPERABLE

--- simple_ffa_run.py
+++ simple_ffa_run.py
@@ -1,14 +1,16 @@
 import pommerman
 from pommerman import agents
-from omri_agent import NewAgent
+from pommerman.agents import PlayerAgent
 
-DEBUG = False
+# from omri_agent import NewAgent
+
+DEBUG = True
 
 
 def main():
     # Print all possible environments in the Pommerman registry
 
-    bla = NewAgent()
+    # bla = NewAgent()
 
     # Create a set of agents (exactly four)
     agent_list = [
@@ -16,7 +18,7 @@ def main():
         agents.RandomAgent(),
         agents.SimpleAgent(),
         # agents.RandomAgent(),
-        NewAgent()
+        PlayerAgent()
         # agents.DockerAgent("pommerman/simple-agent", port=12345),
     ]
     # Make the "Free-For-All" environment using the agent list
@@ -30,15 +32,15 @@ def main():
         while not done:
             if DEBUG and reward[-1] == 0:
                 env.render()
-                print(bla.cur_state)
-                input()
+                # print(bla.cur_state)
+                # input()
             env.render()
             actions = env.act(state)
             state, reward, done, info = env.step(actions)
-            bla.update_q_value(reward[-1])
+            # bla.update_q_value(reward[-1])
 
             # print(reward, done, info)
-        print(bla.q_values)
+        # print(bla.q_values)
         print('Episode {} finished with rewards: {}'.format(i_episode, reward))
     env.close()
 
